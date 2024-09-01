1f7b974d8576af91c0a97f81859030b24a70ea58
NightmareFox <carloshenriquez392004@gmail.com>
2024-09-01 19:33:36 -0400

    add favicon and fix bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'NutriActive',
+      favicon: './src/assets/icon.ico',
       social: {
         github: 'https://github.com/withastro/starlight',
       },
