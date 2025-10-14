434533fd3f56198367a6a7ed8f70544e92e5a7dd
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-10-14 13:21:06 -0400

    feat: add tailwindcss and fix bugs into msg-box

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
     starlight({
       head: [
